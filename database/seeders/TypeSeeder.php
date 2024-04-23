f959411cf5bb99901487933f5843fc3f13843f36
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-23 14:52:18 +0200

    migration for table types created

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = [
+            'Web Development Frontend',
+            'Web Development Backend',
+            'Web Development Full-Stack',
+            'Mock-up and Design',
+        ];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+
+            $newType->name = $type;
+
+            $newType->save();
+        }
     }
 }
