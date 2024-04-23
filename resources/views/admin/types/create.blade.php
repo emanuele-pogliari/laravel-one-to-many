4ad9c39418b969104d28b80e1e267f2c36955d32
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-23 20:48:34 +0200

    Bonus 2 completed.

--- resources/views/admin/types/create.blade.php
+++ resources/views/admin/types/create.blade.php
@@ -7,7 +7,7 @@
             @include('admin/partials/side')
         </div>
         <div id="main-content-edit">
-            <h1>Test edit type page</h1>
+            <h1>Test create type page</h1>
             <div class="container p-5">
                 <h2>Add type</h2>
               <form class="my-5" action="{{route('admin.types.store')}}" method="POST">
