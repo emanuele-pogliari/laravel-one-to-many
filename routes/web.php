d153e63d628e88fc10602f33bd47f506076888f4
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-23 18:04:54 +0200

    created type views

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -43,5 +44,6 @@ Route::middleware(['auth', 'verified'])
 
             Route::get('/', [DashboardController::class, 'index'])->name('index');
             Route::resource('projects', ProjectController::class);
+            Route::resource('types', TypeController::class);
         }
     );
